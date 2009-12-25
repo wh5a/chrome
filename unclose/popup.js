fee5b7114ab7f8ea28a19292cd10c7a2f4ebb150
Wei Hu <weihu@cs.virginia.edu>
2009-12-25 16:21:47 -0500

    Minor UI changes.

--- unclose/popup.js
+++ unclose/popup.js
@@ -27,7 +27,7 @@ function getClosed()
         // Another favicon service is http://www.google.com/s2/favicons?domain=
         stringForThisUrl = "<img src=\"" + "http://getfavicon.appspot.com/" + tabUrl + "\" width=16 height=16 /> ";
 
-      stringForThisUrl += "<a href = \""+ tabUrl + "\" title = \""+ tabUrl + "\" onclick=\"showUrl("+tabId+")\"/>" + localStorage["TabTitle-"+tabId] + "</a><hr>";
+      stringForThisUrl += "<a href = \"javascript:showUrl("+ tabId + ")\"" + "title = \""+ tabUrl + "\" />" + localStorage["TabTitle-"+tabId] + "</a><hr>";
       returnString += stringForThisUrl;
       j++;
     }
@@ -76,5 +76,5 @@ function reset()
   */
   init();
   pageNo = 0;
-  loadContent();
+  window.close();
 }
