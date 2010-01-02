4013b6891d6917790360d95ad7affab00276bbc9
Wei Hu <weihu@cs.virginia.edu>
2010-01-01 22:13:40 -0500

    Better detection.

--- feed/tpgr_cs.js
+++ feed/tpgr_cs.js
@@ -1,11 +1,23 @@
 function isFeed(){
+  // Imprecise detection
   var feedTags=["rss","feed"];
   for(var i in feedTags){
     var elm=document.getElementsByTagName(feedTags[i])[0];
     if(elm&&(!elm.parentElement||elm.parentElement.tagName.toLowerCase()=="body"))
       return true;
   }
-  return false;
+  
+  // Now try to parse the page
+  var req = new XMLHttpRequest();
+  var isF;
+  req.onload = function () {
+    if (req.responseXML) isF = true;
+    else isF = false;
+  }
+  req.onerror = function () {isF = false;}
+  req.open("GET", location.href, false);
+  req.send();
+  return isF;
 }
 
 if(isFeed())
