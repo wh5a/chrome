3a95246c17d552df157ae5a950892d16a5fc8bff
Wei Hu <weihu@cs.virginia.edu>
2010-01-04 04:25:47 -0500

    Manipulate DOM instead of dumping text. Also move popup loading to onmousemove.

--- unclose/popup.js
+++ unclose/popup.js
@@ -1,40 +1,63 @@
 var nItems = 20; // Items in a page
 var pageNo = 0;
 
-function getClosed()
-{		
-  var returnString = "<html><body>";
-  var j = 0;
+function createLink(id, url) {
+  var link = document.createElement('a');
+  link.href = "javascript:showUrl("+ id + ")";
+  link.title = url;
+  return link;
+}
 
+function loadContent()
+{
+  // We only handle mousemove once
+  document.removeEventListener("mousemove", loadContent, false);
+  
+  content = document.getElementById("contentDiv");
+  // Clear
+  while (content.hasChildNodes())
+    content.removeChild(content.firstChild);
+  
   // Drop the first (pageNo*nItems) valid items
-  for(i = localStorage["closeCount"] - 1; i>=0 && j<pageNo*nItems; i--)
+  for (j = 0, i = localStorage["closeCount"] - 1; i>=0 && j<pageNo*nItems; i--)
   {
     tabId = localStorage["ClosedTab-"+i];
     tabUrl = localStorage["TabList-"+tabId];
     if (tabUrl) j++;
   }
 
-  for(j = 0; i>=0 && j<nItems; i --)
+  for (j = 0; i>=0 && j<nItems; i --)
   {
     tabId = localStorage["ClosedTab-"+i];
     tabUrl = localStorage["TabList-"+tabId];
     if (tabUrl) {
-      var stringForThisUrl = "";
-
       // For security concerns, only shows favicon for http pages.
-      if (/^http:/.test(tabUrl))
-        // Another favicon service is http://www.google.com/s2/favicons?domain=
-        stringForThisUrl = "<img src=\"" + "http://getfavicon.appspot.com/" + tabUrl + "\" width=16 height=16 /> ";
+      if (/^http:/.test(tabUrl)) {
+        var img = document.createElement('img');
+        // Other favicon services are http://www.google.com/s2/favicons?domain=
+        // img.src = "http://getfavicon.appspot.com/" + tabUrl;
+        img.src = "http://www.getfavicon.org/?url=" + tabUrl.split('/')[2]; // slower but more powerful. This service only accepts domain names.
+        img.width = 16;
+        img.height = 16;
+        content.appendChild(img);
+      }
 
-      stringForThisUrl += "<a href = \"javascript:showUrl("+ tabId + ")\"" + "title = \""+ tabUrl + "\" />" + localStorage["TabTitle-"+tabId] + "</a><hr>";
-      returnString += stringForThisUrl;
+      // Create a link node and the anchor encapsulating it.
+      var text_link = createLink(tabId, tabUrl);
+      text_link.appendChild(document.createTextNode(" " + localStorage["TabTitle-"+tabId]));
+      content.appendChild(text_link);
+
+      content.appendChild(document.createElement('hr'));
       j++;
     }
   }
-  
-  returnString += "</body></HTML>"	
 
-  return returnString;
+  if (pageNo > 0)
+    document.getElementById("prev").disabled = false;
+  else document.getElementById("prev").disabled = true;
+  if (localStorage["actualCount"] > (pageNo+1) * nItems)
+    document.getElementById("next").disabled = false;
+  else document.getElementById("next").disabled = true;
 }
 
 function next() {
@@ -62,7 +85,7 @@ function showUrl(tabId) {
 
 function reset()
 {
-  for(i = localStorage["closeCount"]-1; i >= 0; i --)
+  for(i = localStorage["closeCount"]-1; i >= 0; i--)
   {
     tabId = localStorage["ClosedTab-"+i];
     delete localStorage["ClosedTab-"+i];
