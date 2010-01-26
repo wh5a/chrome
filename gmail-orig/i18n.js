6699eb2a24752324377e0c352d104bb4cec83da4
Wei Hu <weihu@cs.virginia.edu>
2010-01-25 23:57:18 -0500

    Track Google Mail Checker Plus

---
+++ gmail-orig/i18n.js
@@ -0,0 +1,478 @@
+/*  
+    Google Mail Checker Plus
+    - Localization file -   
+*/
+
+var languages = new Array();
+
+// Arabic
+languages["ar"] = {
+    "id" : "ar",
+    "what" : "عربي",    
+    "months" : new Array("كانون الثاني", "شباط", "آذار", "نيسان", "أيار", "حزيران", "تموز", "آب", "أيلول", "تشرين الأول", "تشرين الثاني", "كانون الأول"),    
+    "readLink" : "مقروءة",
+    "readLinkTitle" : "تعليمها كمقروءة",
+    "unreadLink": "غير مقروءة",
+    "unreadLinkTitle" : "تعليمها كغير مقروءة",
+    "deleteLink" : "حذف",
+    "deleteLinkTitle" : "حذف الرسالة",
+    "spamLink" : "الرسائل المزعج",
+    "spamLinkTitle" : "تعليمها كالرسالة المزعج",
+    "archiveLink" : "الأرشيف",
+    "archiveLinkTitle" : "وضع في الأرشيف",
+    "starLinkTitle" : "الرسائل المميزة بنجمة",
+    "openLinkTitle" : "فتح في جوجل",
+    "replyLinkTitle" : "رد علي الرسالة",
+    "summaryLinkTitle" : "عرض الملخص",
+    "fullLinkTitle" : "عرض  الرسالة كاملة",
+    "refreshLinkTitle" : "تحديث البريد الوارد",
+    "optionsLinkTitle" : "الإعدادات",
+    "composeLinkTitle" : "إنشاء رسالة",
+    "noUnreadText" : "لا يوجد رسائل غير مقروءة",
+    "severalUnreadText" : " رسائل غير مقروءة",
+    "popupText" : "لديك رسائل جديدة!"
+};
+
+// Arabic Western
+languages["arw"] = {
+    "id" : "arw",
+    "what" : "عربي (Western)",    
+    "months" : new Array("يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"),    
+    "readLink" : "مقروءة",
+    "readLinkTitle" : "تعليمها كمقروءة",
+    "unreadLink": "غير مقروءة",
+    "unreadLinkTitle" : "تعليمها كغير مقروءة",
+    "deleteLink" : "حذف",
+    "deleteLinkTitle" : "حذف الرسالة",
+    "spamLink" : "الرسائل المزعج",
+    "spamLinkTitle" : "تعليمها كالرسالة المزعج",
+    "archiveLink" : "الأرشيف",
+    "archiveLinkTitle" : "وضع في الأرشيف",
+    "starLinkTitle" : "الرسائل المميزة بنجمة",
+    "openLinkTitle" : "فتح في جوجل",
+    "replyLinkTitle" : "رد علي الرسالة",
+    "summaryLinkTitle" : "عرض الملخص",
+    "fullLinkTitle" : "عرض  الرسالة كاملة",
+    "refreshLinkTitle" : "تحديث البريد الوارد",
+    "optionsLinkTitle" : "الإعدادات",
+    "composeLinkTitle" : "إنشاء رسالة",
+    "noUnreadText" : "لا يوجد رسائل غير مقروءة",
+    "severalUnreadText" : " رسائل غير مقروءة",
+    "popupText" : "لديك رسائل جديدة!"
+};
+
+// English
+languages["en"] = {
+    "id" : "en",
+    "what" : "English",
+    "months" : new Array("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"),
+    "readLink" : "Read",
+    "readLinkTitle" : "Mark as read",
+    "unreadLink": "Unread",
+    "unreadLinkTitle" : "Mark as unread",
+    "deleteLink" : "Delete",
+    "deleteLinkTitle" : "Delete mail",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Mark as spam",
+    "archiveLink" : "Archive",
+    "archiveLinkTitle" : "Archive mail",
+    "starLinkTitle" : "Star mail",
+    "openLinkTitle" : "Open in webmail",
+    "replyLinkTitle" : "Reply to mail",
+    "summaryLinkTitle" : "Show summary",
+    "fullLinkTitle" : "Show full message",
+    "refreshLinkTitle" : "Refresh all mailboxes",
+    "optionsLinkTitle" : "Go to the options page",
+    "composeLinkTitle" : "Compose new mail",
+    "noUnreadText" : "No unread mail",
+    "oneUnreadText" : "unread mail", // One unread item: "1 unread mail"
+    "severalUnreadText" : "unread mail", // Several unread items: "3 unread mail"
+    "popupText" : "You have received new mail!"
+};
+
+// French - Français
+languages["fr"] = {
+    "id" : "fr",
+    "what" : "Français",
+    "months" : new Array("jan", "fev", "mar", "avr", "mai", "jun", "jul", "aoû", "sep", "oct", "nov", "dec"),
+    "readLink" : "Lu",
+    "readLinkTitle" : "Marquer comme lu",
+    "unreadLink": "Non lu",
+    "unreadLinkTitle" : "Marquer comme non lu",
+    "deleteLink" : "Supprimer",
+    "deleteLinkTitle" : "Supprimer le message",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Signaler comme spam",
+    "archiveLink" : "Archive",
+    "archiveLinkTitle" : "Archiver le message",
+    "starLinkTitle" : "Activer le suivi",
+    "openLinkTitle" : "Ouvrir dans webmail",
+    "replyLinkTitle" : "Répondre",
+    "summaryLinkTitle" : "Afficher le résumé",
+    "fullLinkTitle" : "Afficher le message",
+    "refreshLinkTitle" : "Rafraîchir toutes les boîtes mail",
+    "optionsLinkTitle" : "Allez dans les options",
+    "composeLinkTitle" : "Nouveau message",
+    "noUnreadText" : "Aucun nouveau message",
+    "oneUnreadText" : "nouveau message", // One unread item: "1 unread mail"
+    "severalUnreadText" : "nouveaux messages", // Several unread items: "3 unread mail"
+    "popupText" : "Vous avez reçu un nouveau message !"
+};
+
+// German
+languages["de"] = {
+    "id" : "de",
+    "what" : "Deutsch",
+    "months" : new Array("Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"),
+    "readLink" : "Lesen",
+    "readLinkTitle" : "Als gelesen markieren",
+    "unreadLink": "Ungelesen",
+    "unreadLinkTitle" : "Als ungelesen markieren",
+    "deleteLink" : "Löschen",
+    "deleteLinkTitle" : "Nachricht löschen",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Als Spam markieren",
+    "archiveLink" : "Archivieren",
+    "archiveLinkTitle" : "Nachricht archivieren",
+    "starLinkTitle" : "Star mail",
+    "openLinkTitle" : "Im Browser öffnen",
+    "replyLinkTitle" : "Auf Nachricht antworten",
+    "summaryLinkTitle" : "Zusammenfassung anzeigen",
+    "fullLinkTitle" : "Vollständige Nachricht anzeigen",
+    "refreshLinkTitle" : "Neue Nachrichten abrufen",
+    "optionsLinkTitle" : "Zu den Optionen",
+    "composeLinkTitle" : "Neue E-Mail schreiben",
+    "noUnreadText" : "Keine ungelesenen Nachrichten",
+    "oneUnreadText" : "ungelesene Nachricht", // One unread item: "1 unread mail"
+    "severalUnreadText" : "ungelesene Nachrichten", // Several unread items: "3 unread mail"
+    "popupText" : "Sie haben eine neue Nachricht erhalten!"
+};
+
+// Hebrew
+languages["he"] = {
+    "id" : "he",
+    "what" : "עברית",
+    "months" : new Array("'דצמ'", "נוב'", "אוקט'", "ספט'", "אוג'", "יולי", "יוני", "מאי", "אפר'", "מרץ", "פבר'", "ינו"),
+    "readLink" : "נקרא",
+    "readLinkTitle" : "סמן כנקרא",
+    "unreadLink": "לא נקרא",
+    "unreadLinkTitle" : "סמן כלא נקרא",
+    "deleteLink" : "מחק",
+    "deleteLinkTitle" : "מחק דואר",
+    "spamLink" : "דואר זבל",
+    "spamLinkTitle" : "סמן כדואר זבל",
+    "archiveLink" : "שלח לארכיון",
+    "archiveLinkTitle" : "שלח דואר לארכיון",
+    "starLinkTitle" : "סמן דואר בכוכב",
+    "openLinkTitle" : "Gmailפתח ב",
+    "replyLinkTitle" : "השב לדואר",
+    "summaryLinkTitle" : "הצג סיכום",
+    "fullLinkTitle" : "הצג הודעה במלואה",
+    "refreshLinkTitle" : "רענן את כל תיבות הדואר",
+    "optionsLinkTitle" : "פתח את דף האפשרויות",
+    "composeLinkTitle" : "כתוב דואר",
+    "noUnreadText" : "אין דואר חדש",
+    "oneUnreadText" : "דואר אחד שלא נקרא", // One unread item: "1 unread mail"
+    "severalUnreadText" : "מספר דברי דואר שלא נקראו", // Several unread items: "3 unread mail"
+    "popupText" : "!קיבלת דואר חדש"
+};
+
+// Hungarian
+languages["hu"] = {
+    "id" : "hu",
+    "what" : "Magyar",
+    "months" : new Array("jan", "feb", "már", "ápr", "máj", "jún", "júl", "aug", "szept", "okt", "nov", "dec"),
+    "readLink" : "Olvasott",
+    "readLinkTitle" : "Megjelölés olvasottnak",
+    "unreadLink": "Olvasatlan",
+    "unreadLinkTitle" : "Megjelölés olvasatlannak",
+    "deleteLink" : "Törlés",
+    "deleteLinkTitle" : "Levél törlése",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Megjelölés spamként",
+    "archiveLink" : "Archiválás",
+    "archiveLinkTitle" : "Levél archiválása",
+    "starLinkTitle" : "Levél csillagozása",
+    "openLinkTitle" : "Megnyitás a Gmailben",
+    "replyLinkTitle" : "Válasz küldése",
+    "summaryLinkTitle" : "Összegzés megtekintése",
+    "fullLinkTitle" : "Teljes üzenet megtekintése",
+    "refreshLinkTitle" : "Összes mailbox frissítése",
+    "optionsLinkTitle" : "Ugrás a beállításokhoz",
+    "composeLinkTitle" : "Új levél írása",
+    "noUnreadText" : "Nincs olvasatlan levél",
+    "oneUnreadText" : "olvasatlan levél", // One unread item: "1 unread mail"
+    "severalUnreadText" : "olvasatlan levél", // Several unread items: "3 unread mail"
+    "popupText" : "Új leveled érkezett!"
+};
+
+// Italian
+languages["it"] = {
+    "id" : "it",
+    "what" : "Italiano",
+    "months" : new Array("gen", "feb", "mar", "apr", "mag", "giu", "lug", "ago", "set", "ott", "nov", "dic"),
+    "readLink" : "Letto",
+    "readLinkTitle" : "Segna come letto",
+    "unreadLink": "Da leggere",
+    "unreadLinkTitle" : "Segna come da leggere",
+    "deleteLink" : "Elimina",
+    "deleteLinkTitle" : "Elimina messaggio",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Segnala come spam",
+    "archiveLink" : "Archivia",
+    "archiveLinkTitle" : "Archivia messaggio",
+    "starLinkTitle" : "Segna come speciale",
+    "openLinkTitle" : "Apri in webmail",
+    "replyLinkTitle" : "Rispondi",
+    "summaryLinkTitle" : "Mostra riassunto",
+    "fullLinkTitle" : "Mostra l'intero messaggio",
+    "refreshLinkTitle" : "Aggiorna tutte le caselle",
+    "optionsLinkTitle" : "Vai alla pagina opzioni",
+    "composeLinkTitle" : "Scrivi messaggio",
+    "noUnreadText" : "Nessun nuovo messaggio",
+    "oneUnreadText" : "nuovo messaggio", // One unread item: "1 unread mail"
+    "severalUnreadText" : "nuovi messaggi", // Several unread items: "3 unread mail"
+    "popupText" : "Hai ricevuto un nuovo messaggio!"
+};
+
+// Polish
+languages["pl"] = {
+    "id" : "pl",
+    "what" : "Polski",
+    "months" : new Array("Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"),
+    "readLink" : "Przeczytane",
+    "readLinkTitle" : "Oznacz jako przeczytane",
+    "unreadLink": "Nieprzeczytane",
+    "unreadLinkTitle" : "Oznacz jako nieprzeczytane",
+    "deleteLink" : "Usuń",
+    "deleteLinkTitle" : "Usuń wiadomość",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Oznacz jako spam",
+    "archiveLink" : "Archiwizuj",
+    "archiveLinkTitle" : "Archiwizuj wiadomość",
+    "starLinkTitle" : "Oznacz gwiazdką",
+    "openLinkTitle" : "Otwórz w nowym oknie",
+    "replyLinkTitle" : "Odpowiedz",
+    "summaryLinkTitle" : "Pokaż podsumowanie",
+    "fullLinkTitle" : "Pokaż pełną treść",
+    "refreshLinkTitle" : "Odśwież wszystkie skrzynki",
+    "optionsLinkTitle" : "Przejdź do strony z ustawieniami",
+    "composeLinkTitle" : "Napisz nową wiadomość",
+    "noUnreadText" : "Brak nowych wiadomości",
+    "oneUnreadText" : "nieprzeczytana wiadomość", // One unread item: "1 nieprzeczytana wiadomość"
+    "severalUnreadText" : "nieprzeczytane wiadomości", // Several unread items: "3 nieprzeczytane wiadomości"
+    "popupText" : "Masz nową wiadomość!"
+};
+
+// Russian
+languages["ru"] = {
+    "id" : "ru",
+    "what" : "Русский",
+    "months" : new Array("янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"),
+    "readLink" : "Прочитано",
+    "readLinkTitle" : "Отметить как прочитанное",
+    "unreadLink": "Непрочитано",
+    "unreadLinkTitle" : "Отметить как непрочитанное",
+    "deleteLink" : "Удалить",
+    "deleteLinkTitle" : "Удалить сообщение",
+    "spamLink" : "Спам",
+    "spamLinkTitle" : "Пометить как спам",
+    "archiveLink" : "Архивировать",
+    "archiveLinkTitle" : "Архивировать сообщение",
+    "starLinkTitle" : "Пометить звездочкой",
+    "openLinkTitle" : "Открыть в Gmail",
+    "replyLinkTitle" : "Ответить",
+    "summaryLinkTitle" : "Свернуть сообщение",
+    "fullLinkTitle" : "Показать сообщение полностью",
+    "refreshLinkTitle" : "Проверить почту",
+    "optionsLinkTitle" : "Открыть страницу настроек",
+    "composeLinkTitle" : "Написать письмо",
+    "noUnreadText" : "Нет новых писем",
+    "oneUnreadText" : "новое письмо", // One unread item: "1 unread mail"
+    "severalUnreadText" : "новых письма", // Several unread items: "3 unread mail"
+    "popupText" : "Вы получили новое письмо!"
+};
+
+// Serbian Cyrillic
+languages["sr"] = {
+    "id" : "sr",
+    "what" : "Српски ћирилица",
+    "months" : new Array("јан", "феб", "мар", "апр", "мај", "јун", "јул", "авг", "сеп", "окт", "нов", "дец"),
+    "readLink" : "Прочитано",
+    "readLinkTitle" : "Означи као прочитано",
+    "unreadLink": "Непрочитано",
+    "unreadLinkTitle" : "Означи као непрочитано",
+    "deleteLink" : "Обриши",
+    "deleteLinkTitle" : "Обриши поруку",
+    "spamLink" : "Пријави",
+    "spamLinkTitle" : "Пријави непожељну поруку",
+    "archiveLink" : "Архивирај",
+    "archiveLinkTitle" : "Архивирај поруку",
+    "starLinkTitle" : "Додај звездицу",
+    "openLinkTitle" : "Отвори у новом прозору",
+    "replyLinkTitle" : "Одговори на поруку",
+    "summaryLinkTitle" : "Прикажи кратак преглед",
+    "fullLinkTitle" : "Прикажи целу поруку",
+    "refreshLinkTitle" : "Освежи све налоге",
+    "optionsLinkTitle" : "Иди на подешавања",
+    "composeLinkTitle" : "Састави нову поруку",
+    "noUnreadText" : "Нема непрочитаних порука",
+    "oneUnreadText" : "непрочитана порука", // One unread item: "1 unread mail"
+    "severalUnreadText" : "непрочитане поруке", // Several unread items: "3 unread mail"
+    "popupText" : "Добили сте нову поруку!"
+};
+
+// Serbian Latin
+languages["srl"] = {
+    "id" : "srl",
+    "what" : "Srpski latinica",
+    "months" : new Array("jan", "feb", "mar", "apr", "maj", "jun", "jul", "avg", "sep", "okt", "nov", "dec"),
+    "readLink" : "Pročitano",
+    "readLinkTitle" : "Označi kao pročitano",
+    "unreadLink": "Nepročitano",
+    "unreadLinkTitle" : "Označi kao nepročitano",
+    "deleteLink" : "Obriši",
+    "deleteLinkTitle" : "Obriši poruku",
+    "spamLink" : "Prijavi",
+    "spamLinkTitle" : "Prijavi nepoželjnu poruku",
+    "archiveLink" : "Arhiviraj",
+    "archiveLinkTitle" : "Arhiviraj poruku",
+    "starLinkTitle" : "Dodaj zvezdicu",
+    "openLinkTitle" : "Otvori u novom prozoru",
+    "replyLinkTitle" : "Odgovori na poruku",
+    "summaryLinkTitle" : "Prikaži kratak pregled",
+    "fullLinkTitle" : "Prikaži celu poruku",
+    "refreshLinkTitle" : "Osveži sve naloge",
+    "optionsLinkTitle" : "Idi na podešavanja",
+    "composeLinkTitle" : "Sastavi novu poruku",
+    "noUnreadText" : "Nema nepročitanih poruka",
+    "oneUnreadText" : "nepročitana poruka", // One unread item: "1 unread mail"
+    "severalUnreadText" : "nepročitane poruke", // Several unread items: "3 unread mail"
+    "popupText" : "Dobili ste novu poruku!"
+};
+
+// Slovenian
+languages["sl"] = {
+    "id" : "sl",
+    "what" : "Slovenščina",
+    "months" : new Array("jan", "feb", "mar", "apr", "maj", "jun", "jul", "avg", "sep", "okt", "nov", "dec"),
+    "readLink" : "Prebrano",
+    "readLinkTitle" : "Označi kot prebrano",
+    "unreadLink": "Neprebrano",
+    "unreadLinkTitle" : "Označi kot neprebrano",
+    "deleteLink" : "Izbriši",
+    "deleteLinkTitle" : "Izbriši sporočilo",
+    "spamLink" : "Vsiljena pošta",
+    "spamLinkTitle" : "Označi kot vsiljeno pošto",
+    "archiveLink" : "Arhiviraj",
+    "archiveLinkTitle" : "Arhiviraj sporočilo",
+    "starLinkTitle" : "Označi z zvezdico",
+    "openLinkTitle" : "Odpri v novem zavihku",
+    "replyLinkTitle" : "Odgovori",
+    "summaryLinkTitle" : "Prikaži povzetek",
+    "fullLinkTitle" : "Prikaži celo sporočilo",
+    "refreshLinkTitle" : "Osveži vse",
+    "optionsLinkTitle" : "Odpri stran Možnosti",
+    "composeLinkTitle" : "Novo sporočilo",
+    "noUnreadText" : "Ni neprebranih sporočil",
+    "oneUnreadText" : "neprebrano sporočilo",       // One unread item: "1 unread mail"
+    "severalUnreadText" : "neprebranih sporočil",   // Several unread items: "3 unread mail"
+    "popupText" : "Prejeli ste novo sporočilo!"
+};
+
+// Spanish
+languages["es"] = {
+    "id" : "es",
+    "what" : "Español",
+    "months" : new Array("ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"),
+    "readLink" : "Leído",
+    "readLinkTitle" : "Marcar como leído",
+    "unreadLink": "No leído",
+    "unreadLinkTitle" : "Marcar como no leído",
+    "deleteLink" : "Eliminar",
+    "deleteLinkTitle" : "Eliminar correo",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Marcar como spam",
+    "archiveLink" : "Archivar",
+    "archiveLinkTitle" : "Archivar correo",
+    "starLinkTitle" : "Destacar",
+    "openLinkTitle" : "Abrir en webmail",
+    "replyLinkTitle" : "Responder",
+    "summaryLinkTitle" : "Mostrar resumen",
+    "fullLinkTitle" : "Mostrar mensaje completo",
+    "refreshLinkTitle" : "Consultar nuevos correos",
+    "optionsLinkTitle" : "Ir a la página de opciones",
+    "composeLinkTitle" : "Redactar nuevo correo",
+    "noUnreadText" : "No tienes correos sin leer",
+    "oneUnreadText" : "correo sin leer", // One unread item: "1 unread mail"
+    "severalUnreadText" : "correos sin leer", // Several unread items: "3 unread mail"
+    "popupText" : "Has recibido un nuevo correo!"
+};
+
+// Swedish
+languages["sv"] = {
+    "id" : "sv",
+    "what" : "Svenska",
+    "months" : new Array("jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec"),
+    "readLink" : "Läs",
+    "readLinkTitle" : "Markera som läst",
+    "unreadLink": "Oläs",
+    "unreadLinkTitle" : "Markera som oläst",
+    "deleteLink" : "Radera",
+    "deleteLinkTitle" : "Radera mail",
+    "spamLink" : "Skräppost",
+    "spamLinkTitle" : "Rapportera skräppost",
+    "archiveLink" : "Arkivera",
+    "archiveLinkTitle" : "Arkivera mail",
+    "starLinkTitle" : "Stjärnmärk mail",
+    "openLinkTitle" : "Öppna i webbmail",
+    "replyLinkTitle" : "Svara på mailet",
+    "summaryLinkTitle" : "Visa sammanfattning",
+    "fullLinkTitle" : "Visa hela mailet",
+    "refreshLinkTitle" : "Uppdatera alla mailboxar",
+    "optionsLinkTitle" : "Gå till inställningssidan",
+    "composeLinkTitle" : "Skapa nytt mail",  
+    "noUnreadText" : "Inga olästa mail",
+    "oneUnreadText" : "oläst mail", // One unread item: "1 unread mail"
+    "severalUnreadText" : "olästa mail", // Several unread items: "3 unread mail"
+    "popupText" : "Du har oläst mail!"
+};
+
+// Turkish - Türkçe
+languages["tr"] = {
+    "id" : "tr",
+    "what" : "Türkçe",
+    "months" : new Array("Oca", "Sub", "Mar", "Nis", "May", "Haz", "Tem", "Agu", "Eyl", "Eki", "Kas", "Ara"),
+    "readLink" : "Oku",
+    "readLinkTitle" : "Okundu olarak işaretle",
+    "unreadLink": "Okunmadı",
+    "unreadLinkTitle" : "Okunamadı olarak işaretle",
+    "deleteLink" : "Sil",
+    "deleteLinkTitle" : "Maili sil",
+    "spamLink" : "Spam",
+    "spamLinkTitle" : "Spam olarak işaretle",
+    "archiveLink" : "Arşivle",
+    "archiveLinkTitle" : "Arşiv mail",
+    "starLinkTitle" : "Yıldızlı mail",
+    "openLinkTitle" : "Webmailde aç",
+    "replyLinkTitle" : "Maili yanıtla",
+    "summaryLinkTitle" : "Özet görüntüle",
+    "fullLinkTitle" : "Tümünü görüntüle",
+    "refreshLinkTitle" : "Yenile",
+    "optionsLinkTitle" : "Ayarlar sayfasına git",
+    "composeLinkTitle" : "Yeni mail",
+    "noUnreadText" : "Okunmamış mail yok",
+    "oneUnreadText" : "okunmamış mail", // One unread item: "1 unread mail"
+    "severalUnreadText" : "okunmamış mail", // Several unread items: "3 unread mail"
+    "popupText" : "Yeni bir mail aldınız!"
+};
+
+if(localStorage["gc_language"] == null) {
+    localStorage["gc_language"] = "en";
+}
+
+var lang = languages[localStorage["gc_language"]];
+
+function reloadLanguage() {
+    lang = languages[localStorage["gc_language"]];
+}
